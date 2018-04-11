ee94ae665164d0aabe159151c6b6137db7ac9e87
Henrique Becker <henriquebecker91@gmail.com>
2018-04-11 12:12:44 -0300

    The main file and the ideas file that I forgot to commit.

---
+++ code/cpp/main/test_read.cpp
@@ -0,0 +1,26 @@
+#include <cstdlib>
+
+#include "../lib/darp_instances.hpp"
+
+template <
+  template <typename, typename, typename, typename, typename> class O,
+  typename V,
+  typename L,
+  typename P,
+  typename C,
+  typename T>
+int templated_main(int argc, char** argv) {
+  O<V, L, P, C, T> inst(std::cin);
+  inst.debug_output();
+
+  C bkv;
+  std::vector<set_map_t<L, L>*> bkv_sol;
+  test_all_mv_schedules<O, V, L, P, C, T>(inst, bkv_sol, bkv);
+
+  return EXIT_SUCCESS;
+}
+
+int main(int argc, char** argv) {
+  return templated_main<CL2003_darp_inst_wt, short, short, short, double, double>(argc, argv);
+}
+
