d5166bab060e69ca2a162b5382f4899c3a77e039
Henrique Becker <henriquebecker91@gmail.com>
2019-01-29 00:24:55 -0200

    Adding bare skeleton of extremum_planes.hpp.

---
+++ code/cpp/lib/extremum_planes.hpp
@@ -0,0 +1,22 @@
+#ifndef HBM_EXTREMUM_PLANES_HPP
+#define HBM_EXTREMUM_PLANES_HPP
+
+#include <list>
+
+// A corner point is always the meeting of three planes. However, two
+// distinct boxes can have overlapping box projections, and if they
+// are overlapping in the corner point, then removing one of the boxes will
+// not remove the corner point (even if it is associated to a plane that
+// is associated to that box).
+
+// corner point type
+struct cp_t {
+  
+}
+
+namespace hbm {
+  
+}
+
+#endif // HBM_EXTREMUM_PLANES_HPP
+
