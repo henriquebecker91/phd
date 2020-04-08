fcf2110a0a547d17aebb877151f1c967f2ba3c1b
Henrique Becker <henriquebecker91@gmail.com>
2020-04-07 21:14:02 -0300

    Updated script for preliminar testing.

--- scripts/run_experiment.jl
+++ scripts/run_experiment.jl
@@ -1,13 +1,19 @@
-#!/bin/julia
+#!/bin/bash
+# -*- mode: julia -*-
+#=
+exec julia --project=@. --color=yes --startup-file=no -e "include(popfirst!(ARGS))" "${BASH_SOURCE[0]}" "$@"
+=#
 
 # Not directly used in this script, but needed to enable the glue code in
-# GuillotineModels and make it able to use CPLEX with Requires.jl.
+# GuillotineModels and make it able to use the solver with Requires.jl.
+#import Gurobi
 import CPLEX
 
 import Dates
 import Dates: @dateformat_str
 
-#= Mock GuillotineModels.run used to print debug the script
+# Mock GuillotineModels.run used to check if the script is correct.
+#=
 module GuillotineModels
 	module CommandLine
 		function run(args...; kwargs...)
@@ -19,14 +25,9 @@ end
 =#
 import GuillotineModels
 
-# The file is passed to the block to allow the block to decide when to flush.
-function save_output_in_path(f :: Function, path :: String)
+function save_output_in_path(f, path :: AbstractString)
 	open(path, "a+") do file
-		redirect_stdout(file) do
-			redirect_stderr(file) do
-				f(file)
-			end
-		end
+		redirect_stdout(() -> redirect_stderr(f, file), file)
 	end
 end
 
@@ -42,7 +43,7 @@ function run_batch(
 	, solver         :: String
 	, mock_inst_path :: String
 	, instance_paths :: AbstractVector{String}
-	, options        :: Vector{String} = String[]
+	; options        :: Vector{String} = String[]
 	, solver_seeds   :: Vector{Int} = Int[]
 	, output_path    :: String = Dates.format(
 		Dates.now(), dateformat"yyyy-mm-ddTHH:MM:SS"
@@ -51,30 +52,36 @@ function run_batch(
 	# The mock should not print anything, and even if it prints something,
 	# it will not go into the file (it is not entirely supressed to make
 	# the failure of these assumptions visible).
-	mock_output_flags = ["--no-csv-output", "--$solver-no-output"]
+	mock_output_flags = [
+		"--no-csv-output", "--$solver-no-output", "--$model-quiet"
+	]
 	mock_flags = append!(["--do-not-mock-for-compilation"], mock_output_flags)
 	mock_args = [model, solver, mock_inst_path, mock_flags..., options...]
 	GuillotineModels.CommandLine.run(
-		mock_args; supported_solvers = [Symbol(solver)]
+		mock_args; supported_solvers = [Symbol(solver)],
+		implemented_models = [Symbol(model)]
 	)
-	save_output_in_path(output_path) do io
+	GC.gc()
+	save_output_in_path(output_path) do
 		if isempty(solver_seeds)
 			for inst_path in instance_paths
 				args = append!([model, solver, inst_path], options)
 				GuillotineModels.CommandLine.run(
 					args; supported_solvers = [Symbol(solver)]
 				)
-				flush(io)
+				extra_gc_time = @elapsed GC.gc()
+				@show extra_gc_time
 			end
 		else
 			for inst_path in instance_paths
-				args = append!([model, solver, inst_path], options)
+				non_seed_args = append!([model, solver, inst_path], options)
 				for seed in solver_seeds
-					append!(["--$solver-seed $seed"], args)
+					args = append!(["--$solver-seed", "$seed"], non_seed_args)
 					GuillotineModels.CommandLine.run(
 						args; supported_solvers = [Symbol(solver)]
 					)
-					flush(io)
+					extra_gc_time = @elapsed GC.gc()
+					@show extra_gc_time
 				end
 			end
 		end
@@ -83,8 +90,11 @@ end
 
 function run_experiments(
 	solver = "CPLEX"
-	, instance_folder :: String = "../instances/"
+	; instance_folder :: String = "../instances/"
 	, mock_inst_name :: String = "gcut1"
+	, output_path    :: String = Dates.format(
+		Dates.now(), dateformat"yyyy-mm-ddTHH:MM:SS"
+	) * ".log"
 )
 	mock_inst_path = instance_folder * mock_inst_name
 	all_instance_names = vcat(
@@ -106,6 +116,7 @@ function run_experiments(
 
 	tl = 3600.0
 
+	#=
 	let # just to avoid the variable names from leaking
 		common_flags = ["--do-not-solve", "--PPG2KP-faithful2furini2016"]
 		option_sets = [
@@ -116,31 +127,36 @@ function run_experiments(
 			, ["--PPG2KP-round2disc"]
 		]
 		for options in option_sets
-			append!(options, common_flags)
-			run_batch("PPG2KP", solver, mock_inst_path, all_instance_paths, options)
+			append!(options, common_flags) # note: this changes `option_sets`
+			run_batch(
+				"PPG2KP", solver, mock_inst_path, all_instance_paths, options;
+				output_path = output_path
+			)
 		end
 	end
+	=#
 
 	let # just to avoid the variable names from leaking
-		common_options = ["--$solver-time-limit $tl"]
+		#common_options = ["--$solver-time-limit $tl"]
+		common_options = ["--$solver-time-limit", "$tl", "--PPG2KP-verbose"]
 		option_sets = [
-			String[], ["--PPG2KP-round2disc"],
-			["--PPG2KP-no-cut-position", "--PPG2KP-no-redundant-cut"]
+			String[], ["--PPG2KP-no-pricing"], ["--PPG2KP-faithful2furini2016"],
+			["--PPG2KP-no-pricing", "--PPG2KP-faithful2furini2016"]
+			#["--PPG2KP-round2disc"],
+			#["--PPG2KP-no-cut-position", "--PPG2KP-no-redundant-cut"]
 		]
-		solver_seeds = collect(1:10)
+		solver_seeds = [1, 2, 3] #collect(1:10)
 		for options in option_sets
-			append!(options, common_options)
+			append!(options, common_options) # NOTE: changes `option_sets` elements
 			run_batch(
-				"PPG2KP", solver, mock_inst_path, all_instance_paths, options,
-				solver_seeds
-			)
-			run_batch(
-				"PPG2KP", solver, mock_inst_path, easy_instance_paths,
-				append!(String["--PPG2KP-faithful2furini2016"], options), solver_seeds
+				"PPG2KP", solver, mock_inst_path, easy_instance_paths;
+				options = options, solver_seeds = solver_seeds,
+				output_path = output_path
 			)
 		end
 	end
 end
 
-run_experiments()
+run_experiments("CPLEX")
+run_experiments("Gurobi")
 
