ee32c91dc55f43930d370d65a18d28d0bb20ae71
Henrique Becker <henriquebecker91@gmail.com>
2020-10-10 22:36:58 -0300

    Version submitted to MPC (except to the link to this commit).

--- latex/revised_PPG2KP/compile.sh
+++ latex/revised_PPG2KP/compile.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-pdflatex revised_PPG2KP.tex
+pdflatex --shell-escape revised_PPG2KP.tex
 bibtex revised_PPG2KP
-pdflatex revised_PPG2KP.tex
-pdflatex revised_PPG2KP.tex
+pdflatex --shell-escape revised_PPG2KP.tex
+pdflatex --shell-escape revised_PPG2KP.tex
 
