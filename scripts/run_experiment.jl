f169d987c6bfe36a46a090f48935d115eb3f72d5
Henrique Becker <henriquebecker91@gmail.com>
2021-06-08 15:28:55 -0300

    Updated to include rotation.

--- scripts/run_experiment.jl
+++ scripts/run_experiment.jl
@@ -955,6 +955,10 @@ run(
 
 run(
 	"Gurobi", "G2OPP", "CPG_SSSCSP", Clautiaux42, Clautiaux42[1],
-	"../instances/G2OPP/Clautiaux42/"
+	"../instances/G2OPP/Clautiaux42/";
+	option_sets = [
+		["--PPG2KP-round2disc"],
+		["--PPG2KP-round2disc", "--PPG2KP-allow-rotation", "--PPG2KP-mirror-plates"]
+	]
 )
 
